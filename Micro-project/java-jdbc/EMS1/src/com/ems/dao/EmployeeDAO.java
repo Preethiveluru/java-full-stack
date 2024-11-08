63e29fb9727ea31642e96ae3a78065c6952c0bf5
Veluru <preethi.veluru@biworldwide.com>
2024-11-08 11:31:43 +0530

    coMMON JAVA project

---
+++ Micro-project/java-jdbc/EMS1/src/com/ems/dao/EmployeeDAO.java
@@ -0,0 +1,134 @@
+package com.ems.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.ems.bean.Employee;
+import com.ems.util.DBUtil;
+
+public class EmployeeDAO {
+	public int insertEmployee(Employee employee) {
+		
+		int n =0 ;
+		try {
+		Connection con = DBUtil.getDBCconnection();
+		String sql="insert into employee values(?,?,?)";
+		PreparedStatement ps=  con.prepareStatement(sql);
+		ps.setInt(1, employee.getEid());
+		ps.setString(2, employee.getEname());
+		ps.setInt(3, employee.getEsalary());
+		
+		n = ps.executeUpdate();
+		
+		}catch (Exception e) {
+		
+		}
+		return n;
+		
+	}
+	
+public int deleteEmployee(int eid) {
+		
+		int n =0 ;
+		try {
+		Connection con = DBUtil.getDBCconnection();
+		String sql="delete from employee where eid = ?";
+		PreparedStatement ps=  con.prepareStatement(sql);
+		ps.setInt(1, eid);
+		
+		
+		n = ps.executeUpdate();
+		
+		}catch (Exception e) {
+		
+		}
+		return n;
+		
+	}
+public int updateEmployee(Employee employee) {
+	
+	int n =0 ;
+	try {
+	Connection con = DBUtil.getDBCconnection();
+	String sql="update employee set ename= ?,esalary= ? where eid=? ";
+	PreparedStatement ps=  con.prepareStatement(sql);
+	ps.setString(1, employee.getEname());
+	ps.setInt(2, employee.getEsalary());
+	ps.setInt(3, employee.getEid());
+	
+	n = ps.executeUpdate();
+	
+	}catch (Exception e) {
+	
+	}
+	return n;
+	
+}
+public Employee findEmployee(int eid1) {
+	
+	Employee emp = null;
+	try {
+	Connection con = DBUtil.getDBCconnection();
+	String sql="select*from employee where eid=?";
+	PreparedStatement ps=  con.prepareStatement(sql);
+	ps.setInt(1, eid1);
+	ResultSet rs = ps.executeQuery();
+	if(rs.next())
+		emp = new Employee();
+	    emp.setEid(rs.getInt("eid"));
+	    emp.setEname(rs.getString("ename"));
+	    emp.setEsalary(rs.getInt("esalary"));
+	
+	
+	
+	
+	}catch (Exception e) {
+	
+	}
+	return emp;
+	
+}
+
+public List<Employee> findAllEmployee() {
+	List<Employee> list = new LinkedList<>();
+	
+	try {
+	Connection con = DBUtil.getDBCconnection();
+	String sql="select*from employee ";
+	PreparedStatement ps=  con.prepareStatement(sql);
+	
+	ResultSet rs = ps.executeQuery();
+	while(rs.next()) {
+		Employee emp = new Employee();
+	    emp.setEid(rs.getInt("eid"));
+	    emp.setEname(rs.getString("ename"));
+	    emp.setEsalary(rs.getInt("esalary"));
+	    list.add(emp);
+	
+	}
+	
+	
+	}catch (Exception e) {
+	System.out.println(e);
+	}
+	return list;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
